25dda7d7a2218e318ec15d6875ae43010577e63a
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-21 20:10:18 +0900

    [design] InvalidAccessDescription 마크업
    - 페이지에 올바르지 못한 방법으로 접근할 때 띄울 문구 컴포넌트 마크업.

---
+++ src/components/InvalidAccessDescription/InvalidAccessDescription.jsx
@@ -0,0 +1,7 @@
+import classes from "./InvalidAccessDescription.module.scss";
+
+const InvalidAccessDescription = () => {
+  return <p className={classes.invalidAccess}>잘못된 접근입니다!</p>;
+};
+
+export default InvalidAccessDescription;
