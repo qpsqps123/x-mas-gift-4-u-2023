d78dbc9b216f10c0e18b148ec29d7e6ce94c083e
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-21 20:22:43 +0900

    [feat] question-slice에 새로운 상태 및 리듀서 추가
    - 사용자 입력 값을 담을 inputValue 상태 추가.
    - 사용자 입력이 변할 때마다 실시간으로 상태 값을 업데이트 해주는 리듀서 함수 추가.
    - 본인 확인 및 질문 통과 여부 데이터가 담긴 2중 배열 questionPassed 상태 추가.
    - questionPassed에서 각 문항의 상태 값을 업데이트 해주는 리듀서 함수 추가.

--- src/lib/redux/slices/question-slice.js
+++ src/lib/redux/slices/question-slice.js
@@ -1,11 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUiState = {};
+const initialUiState = {
+  inputValue: null,
+  questionPassed: [
+    ["verifyName", false],
+    ["verifyPhonePwd", false],
+    ["question1", false],
+  ],
+};
 
 const questionSlice = createSlice({
   name: "question",
   initialState: initialUiState,
-  reducers: {},
+  reducers: {
+    changeInputValue(state, action) {
+      state.inputValue = action.payload;
+    },
+    changeNamePassed(state, action) {
+      state.questionPassed[0][1] = action.payload;
+    },
+    changePhonePwdPassed(state, action) {
+      state.questionPassed[1][1] = action.payload;
+    },
+  },
 });
 
 export default questionSlice;
