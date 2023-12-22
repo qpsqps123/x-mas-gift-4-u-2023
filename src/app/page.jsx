d179929077f204190d251465315b8e186ec3b7a0
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-22 12:03:58 +0900

    [chore] 랜딩 페이지 문구 내용 변경

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,8 +13,9 @@ export default function Page() {
     <div>
       <h2 className="a11yHidden">Home</h2>
       <p>
-        오셨군요... 당신만을 위한 크리스마스 선물을 준비해봤습니다.
-        시작해볼까요?
+        오셨군요... 안녕하십니까? 저는 산타입니다. 당신을 위한 크리스마스 선물을 준비했습니다.
+        하지만 선물을 얻기 위해서는 제가 준비한 질문에 올바른 답을 해주셔야만 합니다.
+        자, 시작해 볼까요?
       </p>
       <button onClick={handleClickNext}>다음</button>
     </div>
