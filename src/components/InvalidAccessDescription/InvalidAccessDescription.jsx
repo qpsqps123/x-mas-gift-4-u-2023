b9abda769937dd750ae01cf9395e6329c788a3fb
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-22 12:11:06 +0900

    [feat] 페이지에 잘못된 방식으로 접근 시 랜딩 페이지로 돌아갈 수 있는 링크 추가

--- src/components/InvalidAccessDescription/InvalidAccessDescription.jsx
+++ src/components/InvalidAccessDescription/InvalidAccessDescription.jsx
@@ -1,7 +1,14 @@
+import React from "react";
 import classes from "./InvalidAccessDescription.module.scss";
+import Link from "next/link";
 
 const InvalidAccessDescription = () => {
-  return <p className={classes.invalidAccess}>잘못된 접근입니다!</p>;
+  return (
+    <React.Fragment>
+      <p className={classes.invalidAccess}>잘못된 접근입니다!</p>
+      <Link href="/">처음으로 돌아가기</Link>
+    </React.Fragment>
+  );
 };
 
 export default InvalidAccessDescription;
