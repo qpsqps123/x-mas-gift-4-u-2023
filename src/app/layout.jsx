b8b4d8686415735b9d11e7154f6ae5b3500a1995
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-22 12:09:56 +0900

    [feat] 페이지 루트 레이아웃에 처음으로 돌아가는 링크 이미지 추가
    - 마크업 및 스타일링.
    - 이미지는 임시 이미지로 테스트. 후에 변경 예정.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,8 @@ import classes from "../containers/RootLayout/RootLayout.module.scss";
 import { Noto_Sans_KR } from "next/font/google";
 import StoreProvider from "@/lib/redux/StoreProvider";
 import PageTransition from "@/components/PageTransition/PageTransition";
+import Link from "next/link";
+import Image from "next/image";
 
 const notoSansKr = Noto_Sans_KR({
   subsets: [],
@@ -23,6 +25,15 @@ export default function RootLayout({ children }) {
           <main className={classes.mainWrapper}>
             <h1 className="a11yHidden">X-Mas Gift 4 U</h1>
             <PageTransition />
+            <Link href={"/"}>
+              <Image
+                src="/images/icons/test.png"
+                alt="test"
+                className={classes.homeImg}
+                width={100}
+                height={100}
+              />
+            </Link>
             <div className={classes.mainContainer}>
               <div className={classes.main}>{children}</div>
             </div>
