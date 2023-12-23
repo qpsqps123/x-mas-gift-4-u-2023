460449a8177289a4016adfed36283377c1cb437c
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-23 12:18:18 +0900

    [refactor] 랜딩 페이지 설명 문구 리팩토링
    - 기존 p태그를 Description 컴포넌트로 사용.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Description from "@/components/Description/Description";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
@@ -9,14 +10,20 @@ export default function Page() {
     router.push("/verify-name");
   };
 
+  const descText = (
+    <p>
+      오셨군요... 안녕하십니까? 저는 산타입니다. 당신을 위한 크리스마스 선물을
+      준비했습니다. 하지만 선물을 얻기 위해서는 제가 준비한 질문에 올바른 답을
+      해주셔야만 합니다.
+      <br />
+      자, 시작해 볼까요?
+    </p>
+  );
+
   return (
     <div>
       <h2 className="a11yHidden">Home</h2>
-      <p>
-        오셨군요... 안녕하십니까? 저는 산타입니다. 당신을 위한 크리스마스 선물을 준비했습니다.
-        하지만 선물을 얻기 위해서는 제가 준비한 질문에 올바른 답을 해주셔야만 합니다.
-        자, 시작해 볼까요?
-      </p>
+      <Description text={descText} />
       <button onClick={handleClickNext}>다음</button>
     </div>
   );
