4522d98047fb0c5672fe003a7283e207d3b93f1f
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-23 12:10:05 +0900

    [chore] 커스텀 훅 useValidateProperAccess JSDoc 내용 수정

--- src/hooks/useValidateProperAccess.js
+++ src/hooks/useValidateProperAccess.js
@@ -1,12 +1,16 @@
 import { useCallback } from "react";
 import { useSelector } from "react-redux";
 
+/**
+ * 사용자가 정답 확인 절차를 올바르게 거쳤는지 확인하는 함수를 반환하는 커스텀 훅.
+ * @returns {object} 함수가 든 객체를 반환.
+ */
 const useValidateProperAccess = () => {
   const questionPassed = useSelector((state) => state.question.questionPassed);
 
   /**
    * 올바른 정답 확인 절차를 거쳐서 현재 질문까지 왔는지 확인합니다.
-   * @param {number} currentQuestionNum 현재 질문이 몇 번째 질문인지 기입해주세요.
+   * @param {number} currentQuestionNum 본인 확인 절차 질문을 포함해, 현재까지 거친 질문의 개수를 넣어주세요.
    * @returns {boolean} 올바른 접속이라고 판단되면 true를, 그렇지 않으면 false를 반환합니다.
    */
   const validateProperAccess = useCallback(
