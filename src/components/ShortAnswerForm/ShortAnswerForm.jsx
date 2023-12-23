130fbc534c3da4d19161d5371dcc1a8303efc302
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-23 12:06:11 +0900

    [chore] ShortAnwerForm 컴포넌트 잘못된 이름 정정(->ShortAnswerForm.jsx)

---
+++ src/components/ShortAnswerForm/ShortAnswerForm.jsx
@@ -0,0 +1,21 @@
+"use client";
+import questionSlice from "@/lib/redux/slices/question-slice";
+import { useDispatch } from "react-redux";
+
+const ShortAnswerForm = ({ handleSubmit }) => {
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e) => {
+    dispatch(questionSlice.actions.changeInputValue(e.target.value));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="shortAnswer">답을 작성하고 다음 버튼을 눌러주세요!</label>
+      <input id="shortAnswer" onChange={handleInputChange} />
+      <button>다음</button>
+    </form>
+  );
+};
+
+export default ShortAnswerForm;
