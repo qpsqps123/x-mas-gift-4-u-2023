d143f208bfd103b54690cc6579ce984cccf9a1f4
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-23 12:14:12 +0900

    [feat] question-slice에 문제와 관련된 상태 및 리듀서 함수 추가
    - 기존 question1 상태와 question1Passed 리듀서 함수와 동일한 형식으로 question2 ~ question10까지 추가.
    - changeNamePassed 리듀서 함수와 changePhonePwdPassed 리듀서 함수의 이름 변경.

--- src/lib/redux/slices/question-slice.js
+++ src/lib/redux/slices/question-slice.js
@@ -6,6 +6,15 @@ const initialUiState = {
     ["verifyName", false],
     ["verifyPhonePwd", false],
     ["question1", false],
+    ["question2", false],
+    ["question3", false],
+    ["question4", false],
+    ["question5", false],
+    ["question6", false],
+    ["question7", false],
+    ["question8", false],
+    ["question9", false],
+    ["question10", false],
   ],
 };
 
@@ -16,11 +25,41 @@ const questionSlice = createSlice({
     changeInputValue(state, action) {
       state.inputValue = action.payload;
     },
-    changeNamePassed(state, action) {
+    nameVerificationPassed(state, action) {
       state.questionPassed[0][1] = action.payload;
     },
-    changePhonePwdPassed(state, action) {
+    phonePwdVerificationPassed(state, action) {
       state.questionPassed[1][1] = action.payload;
+    },
+    question1Passed(state, action) {
+      state.questionPassed[2][1] = action.payload;
+    },
+    question2Passed(state, action) {
+      state.questionPassed[3][1] = action.payload;
+    },
+    question3Passed(state, action) {
+      state.questionPassed[4][1] = action.payload;
+    },
+    question4Passed(state, action) {
+      state.questionPassed[5][1] = action.payload;
+    },
+    question5Passed(state, action) {
+      state.questionPassed[6][1] = action.payload;
+    },
+    question6Passed(state, action) {
+      state.questionPassed[7][1] = action.payload;
+    },
+    question7Passed(state, action) {
+      state.questionPassed[8][1] = action.payload;
+    },
+    question8Passed(state, action) {
+      state.questionPassed[9][1] = action.payload;
+    },
+    question9Passed(state, action) {
+      state.questionPassed[10][1] = action.payload;
+    },
+    question10Passed(state, action) {
+      state.questionPassed[11][1] = action.payload;
     },
   },
 });
