a556771c6ef4739cf1310f5210c95a07a78dd10e
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-24 15:31:27 +0900

    [refactor] 동적 라우팅 사용과 정적 데이터 파일을 추가함에 맞춰 코드 리팩토링

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 "use client";
 import Description from "@/components/Description/Description";
+import { desc } from "@/constants/desc";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
@@ -7,23 +8,13 @@ export default function Page() {
 
   const handleClickNext = (e) => {
     e.preventDefault();
-    router.push("/verify-name");
+    router.push("/verify/name");
   };
-
-  const descText = (
-    <p>
-      오셨군요... 안녕하십니까? 저는 산타입니다. 당신을 위한 크리스마스 선물을
-      준비했습니다. 하지만 선물을 얻기 위해서는 제가 준비한 질문에 올바른 답을
-      해주셔야만 합니다.
-      <br />
-      자, 시작해 볼까요?
-    </p>
-  );
 
   return (
     <div>
       <h2 className="a11yHidden">Home</h2>
-      <Description text={descText} />
+      <Description text={desc["home"]} />
       <button onClick={handleClickNext}>다음</button>
     </div>
   );
