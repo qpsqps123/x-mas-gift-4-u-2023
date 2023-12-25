aae6b062855945bb1ae510a944545be6a9386940
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-25 20:49:36 +0900

    [fix] 객관식 9번 레이아웃 문제로 Description 컴포넌트 수정
    - 객관식 9번 페이지로 라우팅 되었을 떄만 폰트 사이즈를 rem(12px)로 적용.

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,8 +1,19 @@
+"use client";
 import React from "react";
 import classes from "./Description.module.scss";
+import { usePathname } from "next/navigation";
 
 const Description = ({ text }) => {
-  return <div className={classes.textContainer}>{text}</div>;
+  const pathName = usePathname();
+  return (
+    <div
+      className={`${classes.textContainer} ${
+        pathName === "/question/mc/9" ? classes.smallFont : null
+      }`}
+    >
+      {text}
+    </div>
+  );
 };
 
 export default Description;
