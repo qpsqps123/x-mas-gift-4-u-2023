9507985ea81e141d1249bf68ba25be31a9cb149f
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-25 21:10:57 +0900

    [design] 주관식 문제 입력 양식 스타일링을 위한 마크업

--- src/components/ShortAnswerForm/ShortAnswerForm.jsx
+++ src/components/ShortAnswerForm/ShortAnswerForm.jsx
@@ -1,6 +1,8 @@
 "use client";
+import classes from "./ShortAnswerForm.module.scss";
 import questionSlice from "@/lib/redux/slices/question-slice";
 import { useDispatch } from "react-redux";
+import NextPageButton from "../NextPageButton/NextPageButton";
 
 const ShortAnswerForm = ({ handleSubmit }) => {
   const dispatch = useDispatch();
@@ -10,10 +12,16 @@ const ShortAnswerForm = ({ handleSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="shortAnswer">답을 작성하고 다음 버튼을 눌러주세요!</label>
-      <input id="shortAnswer" onChange={handleInputChange} />
-      <button>다음</button>
+    <form id="shortAnswerForm" onSubmit={handleSubmit} className={classes.form}>
+      <label htmlFor="shortAnswer" className={classes.label}>
+        답을 작성하고 다음 버튼을 눌러주세요!
+      </label>
+      <input
+        id="shortAnswer"
+        className={classes.input}
+        onChange={handleInputChange}
+      />
+      <NextPageButton form={"shortAnswerForm"} />
     </form>
   );
 };
