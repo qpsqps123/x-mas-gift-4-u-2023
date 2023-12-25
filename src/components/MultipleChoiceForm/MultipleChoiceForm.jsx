1b69272245fd6c1c918398cd1bab2e5bc8e299dc
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-25 21:07:32 +0900

    [design] 객관식 입력 양식 컴포넌트 스타일링을 위한 마크업

--- src/components/MultipleChoiceForm/MultipleChoiceForm.jsx
+++ src/components/MultipleChoiceForm/MultipleChoiceForm.jsx
@@ -1,65 +1,73 @@
 "use client";
+
+import React from "react";
+import { useDispatch } from "react-redux";
+import classes from "./MultipleChoiceForm.module.scss";
+import NextPageButton from "../NextPageButton/NextPageButton";
 import questionSlice from "@/lib/redux/slices/question-slice";
-import { useDispatch, useSelector } from "react-redux";
 
 const MultipleChoiceForm = ({ handleSubmit, ...rest }) => {
   const dispatch = useDispatch();
 
   const choicesObj = { ...rest };
 
-  const inputValue = useSelector((state) => state.question.inputValue);
   const handleInputChange = (e) => {
     dispatch(questionSlice.actions.setInputValue(e.target.value));
-    console.log(inputValue);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <fieldset>
-        <legend>답을 선택하고 다음 버튼을 누르세요.</legend>
-        <div>
-          <label htmlFor="choice1">{choicesObj.choice1}</label>
-          <input
-            type="radio"
-            id="choice1"
-            name="choices"
-            onChange={handleInputChange}
-            value={choicesObj.choice1}
-          />
-        </div>
-        <div>
-          <label htmlFor="choice2">{choicesObj.choice2}</label>
-          <input
-            type="radio"
-            id="choice2"
-            name="choices"
-            onChange={handleInputChange}
-            value={choicesObj.choice2}
-          />
-        </div>
-        <div>
-          <label htmlFor="choice3">{choicesObj.choice3}</label>
-          <input
-            type="radio"
-            id="choice3"
-            name="choices"
-            onChange={handleInputChange}
-            value={choicesObj.choice3}
-          />
-        </div>
-        <div>
-          <label htmlFor="choice4">{choicesObj.choice4}</label>
-          <input
-            type="radio"
-            id="choice4"
-            name="choices"
-            onChange={handleInputChange}
-            value={choicesObj.choice4}
-          />
-        </div>
-      </fieldset>
-      <button>다음</button>
-    </form>
+    <React.Fragment>
+      <form
+        id="multipleChoiceForm"
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <fieldset className={classes.fieldset}>
+          <legend>답을 선택하고 다음 버튼을 누르세요.</legend>
+          <div className={classes.inputAndLabelContainer}>
+            <input
+              type="radio"
+              id="choice1"
+              name="choices"
+              onChange={handleInputChange}
+              value={choicesObj.choice1}
+            />
+            <label htmlFor="choice1">{choicesObj.choice1}</label>
+          </div>
+          <div className={classes.inputAndLabelContainer}>
+            <input
+              type="radio"
+              id="choice2"
+              name="choices"
+              onChange={handleInputChange}
+              value={choicesObj.choice2}
+            />
+            <label htmlFor="choice2">{choicesObj.choice2}</label>
+          </div>
+          <div className={classes.inputAndLabelContainer}>
+            <input
+              type="radio"
+              id="choice3"
+              name="choices"
+              onChange={handleInputChange}
+              value={choicesObj.choice3}
+            />
+            <label htmlFor="choice3">{choicesObj.choice3}</label>
+          </div>
+          <div className={classes.inputAndLabelContainer}>
+            <input
+              type="radio"
+              id="choice4"
+              name="choices"
+              onChange={handleInputChange}
+              value={choicesObj.choice4}
+            />
+            <label htmlFor="choice4">{choicesObj.choice4}</label>
+          </div>
+        </fieldset>
+      </form>
+      <NextPageButton form={"multipleChoiceForm"} />
+    </React.Fragment>
   );
 };
 
