b0e1eedc471c3515ae3652a16dc779ceadfa6052
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-25 20:58:09 +0900

    [design] 오답 알림 컴포넌트(InvalidAnswerDescription.jsx) 마크업 수정
    - div 래퍼 태그 추가.
    - 스타일링을 위한 마크업.

--- src/components/InvalidAnswerDescription/InvalidAnswerDescription.jsx
+++ src/components/InvalidAnswerDescription/InvalidAnswerDescription.jsx
@@ -1,5 +1,11 @@
+import classes from "./InvalidAnswerDescription.module.scss";
+
 const InvalidAnswerDescription = () => {
-  return <p>입력하신 값이 올바르지 않습니다!</p>;
+  return (
+    <div className={classes.container}>
+      <p className={classes.text}>입력하신 값이 올바르지 않습니다!</p>
+    </div>
+  );
 };
 
 export default InvalidAnswerDescription;
