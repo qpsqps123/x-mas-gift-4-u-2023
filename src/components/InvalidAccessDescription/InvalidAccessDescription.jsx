09f6693f673b2f3c7dea15b9d0bdd2a28999346d
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-25 20:55:06 +0900

    [feat] 잘못된 접근 표시 컴포넌트(InvalidAccessDescription 컴포넌트) 링크 태그 추가 및 스타일링 마크업
    - 랜딩페이지로 돌아갈 때는 캐시 데이터를 비워야 하므로, Next.js Link 컴포넌트는 대신 a 태그 사용.
    - 스타일링을 위한 마크업.

--- src/components/InvalidAccessDescription/InvalidAccessDescription.jsx
+++ src/components/InvalidAccessDescription/InvalidAccessDescription.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import classes from "./InvalidAccessDescription.module.scss";
-import Link from "next/link";
 
 const InvalidAccessDescription = () => {
   return (
     <React.Fragment>
-      <p className={classes.invalidAccess}>잘못된 접근입니다!</p>
-      <Link href="/">처음으로 돌아가기</Link>
+      <div className={classes.invalidAccessContainer}>
+        <p className={classes.invalidAccess}>잘못된 접근입니다!</p>
+        <a href="/" className={classes.homeLink}>
+          처음으로 돌아가기
+        </a>
+      </div>
     </React.Fragment>
   );
 };
