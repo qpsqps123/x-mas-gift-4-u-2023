5f3d5f73e214d8e75dfe028873fcdcbbe7420234
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-12-25 21:18:32 +0900

    [feat] 화면 전환 컴포넌트 수정
    - 커튼을 삼중으로 설정(back, mid, front).
    - 랜딩페이지 애니메이션을 위한 요소 추가.
    - 스타일링을 위한 마크업.

--- src/components/PageTransition/PageTransition.jsx
+++ src/components/PageTransition/PageTransition.jsx
@@ -1,11 +1,33 @@
+"use client";
 import React from "react";
 import classes from "./PageTransition.module.scss";
+import { Mountains_of_Christmas } from "next/font/google";
+import { usePathname } from "next/navigation";
+
+const mountainsOfChristmas = Mountains_of_Christmas({
+  weight: ["400"],
+  subsets: ["latin"],
+  fallback: ["serif"],
+  variable: "--mountains-of-christmas",
+});
 
 const PageTransition = () => {
+  const pathname = usePathname();
+
   return (
     <React.Fragment>
-      <div className={classes.pageTransitionBackground}></div>
-      <div className={classes.pageTransitionForeground}></div>
+      <div id="backCurtain" className={classes.backCurtain}></div>
+      <div id="midCurtain" className={classes.midCurtain}></div>
+      <div id="frontCurtain" className={classes.frontCurtain}></div>
+      {pathname === "/" && (
+        <div id="homePageCurtain" className={classes.homePageCurtain}>
+          <p
+            className={`${mountainsOfChristmas.className} ${classes.homePageTitle}`}
+          >
+            X-Mas Gift 4 U.
+          </p>
+        </div>
+      )}
     </React.Fragment>
   );
 };
